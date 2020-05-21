8567e38f5224a1dcd3467f560c21a4ae21c0110d
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-21 21:44:46 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,16 +1,6 @@
 
 def count_strings(array)
-  total = 0 
-  index = 0 
-  while index < array.length do 
-    array.count do |object| 
-      if array[object].is_a?(String)
-        total += 1 
-      end 
-      end 
-      index += 1 
-    end 
-total 
+ 
 end
 
 def count_empty_strings(array)
